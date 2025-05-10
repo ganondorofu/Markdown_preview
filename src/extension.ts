2bd17a69a32fb861e326fb03d97c80a99255f9a6
ganondorofu <167788682+ganondorofu@users.noreply.github.com>
2025-05-10 01:19:42 +0000

    アクティビティバーに何もないです

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,11 @@
-import type * as vscode from 'vscode';
+
+import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+    console.log('Congratulations, your extension "popup-pro" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
